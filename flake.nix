c4948506bb96757ed0602f731737be15c7149e24
HikaruEgashira <account@egahika.dev>
2025-10-18 14:25:14 +0900

    simple

--- flake.nix
+++ flake.nix
@@ -11,20 +11,16 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
-        # Library package (suggester/)
         lm-suggester-lib = pkgs.buildGoModule {
           pname = "lm-suggester-lib";
           version = "0.0.0-dev";
 
           src = ./.;
 
-          # Use lib.fakeHash initially, then replace with actual hash after first build
           vendorHash = "sha256-Z+o55vhuhHe2kJbPjCKVmTY8gzXR6TjbaIUdaRaFEho=";
 
-          # Don't install any binaries for the library package
           subPackages = [ ];
 
-          # Skip installation phase since this is just a library
           doInstallCheck = false;
 
           meta = with pkgs.lib; {
@@ -35,19 +31,16 @@
           };
         };
 
-        # CLI package (cmd/lm-suggester/)
         lm-suggester-cli = pkgs.buildGoModule {
           pname = "lm-suggester";
           version = "0.0.0-dev";
 
           src = ./.;
 
-          # CLI has its own go.mod in cmd/lm-suggester/
           modRoot = "./cmd/lm-suggester";
 
           vendorHash = "sha256-IwBucCfotipyZKw0uil9gChEkG0KeZNRUr2ggm08wX4=";
 
-          # Build only the CLI binary
           subPackages = [ "." ];
 
           ldflags = [
@@ -66,14 +59,12 @@
         };
       in
       {
-        # Packages output
         packages = {
           lib = lm-suggester-lib;
           cli = lm-suggester-cli;
           default = lm-suggester-cli;
         };
 
-        # Development shell
         devShells.default = pkgs.mkShellNoCC {
           packages = with pkgs; [
             go_1_25
@@ -83,7 +74,6 @@
             git
             syft
 
-            # base utilities
             bashInteractive
             coreutils
           ];
